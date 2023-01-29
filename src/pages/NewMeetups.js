25dc4a057212e5635004b5cb32ad39184e31d23a
echomonk <erdaloylupinar@gmail.com>
2023-01-29 07:44:34 +0300

    created new meetup component and POST request

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,29 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-  return <div>New Meetups</div>;
+  const history = useHistory();
+
+  function handleAddMeetup(meetupData) {
+    fetch(
+      "https://react-meetups-941e9-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1> Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
 }
 
 export default NewMeetupsPage;
